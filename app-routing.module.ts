fe3e10ad055d6ea4cf4d4756ae70c5c3da298bf5
RIVALDI AMDC <85064601+rivaldiarvanda@users.noreply.github.com>
2023-01-17 11:04:34 +0700

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { CuacaComponent } from './cuaca/cuaca.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { OtentikasiGuard } from './otentikasi.guard';
+
+const routes: Routes = [
+{path: "", redirectTo : "login", pathMatch : "full" },
+{path: "admin", component : AdminComponent },
+{path: "cuaca", component : CuacaComponent, canActivate : [OtentikasiGuard] },
+{path: "dashboard", component : DashboardComponent, canActivate : [OtentikasiGuard] },
+{path: "dashboard2", component : Dashboard2Component, canActivate : [OtentikasiGuard] },
+{path: "forex", component : ForexComponent, canActivate : [OtentikasiGuard] },
+{path: "login", component : LoginComponent },
+{path: "mahasiswa", component : MahasiswaComponent, canActivate : [OtentikasiGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { ForexComponent } from './forex/forex.component';
+import { CuacaComponent } from './cuaca/cuaca.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    AdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
+    MahasiswaComponent,
+    Dashboard2Component,
+    ForexComponent,
+    CuacaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
